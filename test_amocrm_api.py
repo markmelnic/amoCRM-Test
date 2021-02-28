c3f34fb232acd016d4b46d0877e37ffe8ce5f6c2
Mark Melnic <markhappy200@gmail.com>
2021-02-28 20:46:52 +0200

    Working amocrm_api test

--- test_amocrm_api.py
+++ test_amocrm_api.py
@@ -5,6 +5,7 @@ from amocrm_api import AmoOAuthClient
 access_token = open("access_token.txt", "r").read()
 refresh_token = open("refresh_token.txt", "r").read()
 
-client = AmoOAuthClient(access_token, refresh_token, "https://"+getenv('SUBDOMAIN'), getenv('CLIENT_ID'), getenv('CLIENT_SECRET'), getenv('REDIRECT'))
+client = AmoOAuthClient(access_token, refresh_token, getenv('F_SUBDOMAIN'), getenv('CLIENT_ID'), getenv('CLIENT_SECRET'), getenv('REDIRECT'))
 
-info = client.get_account_info()
+info = client.get_account_info(with_amojo_id=True)
+print(info)
